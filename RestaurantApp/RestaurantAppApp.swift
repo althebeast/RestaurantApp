7cac35df083b5acc69dab809fe442bef6cd3be7b
althebeast <alperensarisan@icloud.com>
2023-12-06 23:25:42 -0400

    Initial Commit

---
+++ RestaurantApp/RestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppApp.swift
+//  RestaurantApp
+//
+//  Created by Alperen Sarışan on 6.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
