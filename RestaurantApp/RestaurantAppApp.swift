89cf19fbcbacdf47212e587d837440db4d52c72f
Alperen <alperensarisan@icloud.com>
2024-02-02 22:21:18 -0400

    Initial Commit

---
+++ RestaurantApp/RestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppApp.swift
+//  RestaurantApp
+//
+//  Created by Alperen Sarışan on 2.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
